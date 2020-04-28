e6fc9c7062f39996c5ea0da0b2790856ddffc1b4
DhiaKhalfallah <47749870+DhiaKhalfallah@users.noreply.github.com>
2020-04-28 05:15:28 +0200

    Add files via upload
    Chart for claim type

---
+++ Dhia Khalfallah/Backend/Gra_rec.php
@@ -0,0 +1,327 @@
+<?php
+  include "entities/reclamation.php";
+          include "core/reclam.php";
+   $reclam1=new Reclam();
+           $listeCommandes=$reclam1->afficherReclam();
+$i=0;
+$j=0;
+$l=0;
+?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+
+    <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+   <!-- Left Panel -->
+
+   <aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
+            <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
+        </div>
+
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i> Espace Admin </a>
+                </li>
+                <h3 class="menu-title"> Éléments IU</h3><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <!-- <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li> -->
+                        <li><i class="fa fa-table"></i><a href="table-client.html">Client</a></li>
+                        <li><i class="fa fa-table"></i><a href="table-panier.html">Panier</a></li>
+                        <li><i class="fa fa-table"></i><a href="table-produit.html">Produit</a></li>
+                        <li><i class="fa fa-table"></i><a href="table-coaching.html">Coaching</a></li>
+                        <li><i class="fa fa-table"></i><a href="table-marketing.html">Marketing</a></li>
+                        <li><i class="fa fa-table"></i><a href="table-service.html">Service après-vente</a></li>
+                    </ul>
+                </li>
+            
+
+                <h3 class="menu-title">Icons</h3><!-- /.menu-title -->
+                <li>
+                    <a href="widgets.html"> <i class="menu-icon ti-email"></i>Widgets </a>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Graphiques</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-area-chart"></i><a href="Gra_offres.html"> GraphiqueProduit</a></li>
+                    </ul>
+                </li>
+                
+                <h3 class="menu-title">Suppléments</h3><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="page-login.html">S'identifier</a></li>
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="page-register.html">S'inscrire</a></li>
+                        <li><i class="menu-icon fa fa-paper-plane"></i><a href="pages-forget.html">Mot De Passe Oublier</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside><!-- /#left-panel -->
+
+<!-- Left Panel -->
+
+<!-- Right Panel -->
+
+<div id="right-panel" class="right-panel">
+
+    <!-- Header-->
+    <header id="header" class="header">
+
+        <div class="header-menu">
+
+            <div class="col-sm-7">
+                <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                <div class="header-left">
+                    <button class="search-trigger"><i class="fa fa-search"></i></button>
+                    <div class="form-inline">
+                        <form class="search-form">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
+                            <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
+                        </form>
+                    </div>
+
+                    <div class="dropdown for-notification">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-bell"></i>
+                            <span class="count bg-danger">5</span>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="notification">
+                            <p class="red">vous avez 3 Notifications</p>
+                            <a class="dropdown-item media bg-flat-color-1" href="#">
+                            <i class="fa fa-check"></i>
+                            <p>Serveur # 1 surchargé.</p>
+                        </a>
+                            <a class="dropdown-item media bg-flat-color-4" href="#">
+                            <i class="fa fa-info"></i>
+                            <p>Serveur #2  surchargé.</p>
+                        </a>
+                            <a class="dropdown-item media bg-flat-color-5" href="#">
+                            <i class="fa fa-warning"></i>
+                            <p>Serveur # 1 surchargé.</p>
+                        </a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown for-message">
+                        <button class="btn btn-secondary dropdown-toggle" type="button"
+                            id="message"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="ti-email"></i>
+                            <span class="count bg-primary">9</span>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="message">
+                            <p class="red">vous avez 4 Mails</p>
+                            <a class="dropdown-item media bg-flat-color-1" href="#">
+                            <span class="photo media-left"><img alt="avatar" src="images/avatar/1.jpg"></span>
+                            <span class="message media-body">
+                                <span class="name float-left">Jonathan Smith</span>
+                                <span class="time float-right"> Juste maintenant </span>
+                                    <p>salut,c'est un  example msg</p>
+                            </span>
+                        </a>
+                            <a class="dropdown-item media bg-flat-color-4" href="#">
+                            <span class="photo media-left"><img alt="avatar" src="images/avatar/2.jpg"></span>
+                            <span class="message media-body">
+                                <span class="name float-left">Jack Sanders</span>
+                                <span class="time float-right"> Il y a 5 minutes</span>
+                                    <p>salut,c'est un  example msg</p>
+                            </span>
+                        </a>
+                            <a class="dropdown-item media bg-flat-color-5" href="#">
+                            <span class="photo media-left"><img alt="avatar" src="images/avatar/3.jpg"></span>
+                            <span class="message media-body">
+                                <span class="name float-left">Cheryl Wheeler</span>
+                                <span class="time float-right"> Il y a 10 minutes </span>
+                                    <p>salut,c'est un  example msg</p>
+                            </span>
+                        </a>
+                            <a class="dropdown-item media bg-flat-color-3" href="#">
+                            <span class="photo media-left"><img alt="avatar" src="images/avatar/4.jpg"></span>
+                            <span class="message media-body">
+                                <span class="name float-left">Rachel Santos</span>
+                                <span class="time float-right">Il y a 15 minutes </span>
+                                    <p>salut,c'est un  example msg</p>
+                            </span>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-5">
+                <div class="user-area dropdown float-right">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
+                    </a>
+
+                    <div class="user-menu dropdown-menu">
+                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Mon Profile</a>
+
+                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Notifications <span class="count">13</span></a>
+
+                        <a class="nav-link" href="#"><i class="fa fa-cog"></i> paramétre</a>
+
+                        <a class="nav-link" href="#"><i class="fa fa-power-off"></i> Se déconnecter</a>
+                    </div>
+                </div>
+
+                <div class="language-select dropdown" id="language-select">
+                    <a class="dropdown-toggle" href="#" data-toggle="dropdown"  id="language" aria-haspopup="true" aria-expanded="true">
+                        <i class="flag-icon flag-icon-us"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="language">
+                        <div class="dropdown-item">
+                            <span class="flag-icon flag-icon-fr"></span>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="flag-icon flag-icon-es"></i>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="flag-icon flag-icon-us"></i>
+                        </div>
+                        <div class="dropdown-item">
+                            <i class="flag-icon flag-icon-it"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </header><!-- /header -->
+
+ 
+    <div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>Espace Admin</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="page-header float-right">
+                <div class="page-title">
+                    <ol class="breadcrumb text-right">
+                        <li><a href="#">Espace Admin</a></li>
+                        <li><a href="#">Table</a></li>
+                        <li class="active"> table de donnée  </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+foreach($listeCommandes as $row){
+if($row['sujet']=='Produit(s) endommagé(s)'){
+    $i++;
+}
+else if($row['sujet']=='Probléme de livraison'){
+    $j++;
+}else
+if($row['sujet']=='Produit(s) manquant(s)'){
+    $l++;
+}
+
+
+}
+
+
+ 
+ $dataPoints = array(
+     array("label"=> "Produit(s) endommagé(s)", "y"=>$i ),
+     array("label"=> "Probléme de livraison", "y"=> $l),
+     array("label"=> "Produit(s) manquant(s)", "y"=> $j),
+ 
+ );
+     
+ ?>
+
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	title:{
+		text: "Les types de réclamations"
+	},
+	subtitles: [{
+		text: "Le nombre total "
+	}],
+	data: [{
+		type: "pie",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 16,
+		indexLabel: "{label} - #percent%",
+		yValueFormatString: "฿#,##0",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js
+"></script>
+
+</body>
+
+</html>
+<script>
+Morris.Bar({
+ element : 'chart',
+ data:[<?php echo $chart_data; ?>],
+ xkey:'pour_reduc',
+ //ykeys:['prix_total'],
+ ykeys:['ref_off', 'id_prod'],
+ labels:['ref_off', 'id_prod'],
+ hideHover:'auto',
+ stacked:true
+});
+</script>
