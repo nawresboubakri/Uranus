475836317c669ad4bcaacba8fb38aa98868f71af
DhiaKhalfallah <47749870+DhiaKhalfallah@users.noreply.github.com>
2020-05-31 14:09:08 +0200

    Add files via upload

--- Dhia Khalfallah/Frontend/ajoutRec.php
+++ Dhia Khalfallah/Frontend/ajoutRec.php
@@ -1,6 +1,9 @@
 <?PHP
-include "entities/reclamation.php";
-include "core/reclam.php";
+include "../../entities/reclamation.php";
+include "../../core/reclam.php";
+
+$body="Votre reclamation a été recu et sera traiter dans les plus bref delais. Merci pour votre confiance.";
+
 
 if ( !(empty($_POST['mail'])) && !(empty($_POST['sujet'])) && !(empty($_POST['details'])) && !(empty($_POST['idc'])) )
 {
@@ -11,8 +14,11 @@ if ( !(empty($_POST['mail'])) && !(empty($_POST['sujet'])) && !(empty($_POST['de
 
 $reclam1c=new Reclam();
 $reclam1c->ajouterReclam($reclam1);
-	
+  
 
+mail($_POST['mail'], $id, $body, 'From: serviceclients.sport@gmail.com');
+
+ 
 }
 else
 {
@@ -23,6 +29,6 @@ echo "<script type='text/javascript'>alert('$message');</script>";
 }else{
 	echo "Vérifier les champs";
 }
-//*/
+header('Location: contact.php');
 
 ?>
